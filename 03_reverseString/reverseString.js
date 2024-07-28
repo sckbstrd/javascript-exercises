97f7e0d45ced4ce1241b9d36da52c4ca6cef1ff7
sckbstrd <jguitarok@gmail.com>
2024-07-28 18:27:51 +0300

    Solve reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringLetters = string.split("");
+    const reversedLetters = stringLetters.reverse();
+    return reversedLetters.join("");
 };
 
 // Do not edit below this line
