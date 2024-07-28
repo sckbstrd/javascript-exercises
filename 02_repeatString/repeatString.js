7f9b084c79c39d848a6451315a4f952c46c09705
sckbstrd <jguitarok@gmail.com>
2024-07-28 18:20:25 +0300

    Solve repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,9 @@
-const repeatString = function() {
+const repeatString = function(string, times) {
+    if (times >= 0) {
+        return string.repeat(times);
+    } else {
+        return "ERROR";
+    }
 
 };
 
