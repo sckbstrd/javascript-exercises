bab56626dbf3df4bd7524972687fec062e79b621
sckbstrd <jguitarok@gmail.com>
2024-07-30 15:45:15 +0300

    Solve removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    for (let value of values) {
+        let index;
+        while ((index = array.indexOf(value)) !== -1) {
+            array.splice(index, 1);
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
