90bb10b9b3035d47638648705ee8ae6b84d2607a
sckbstrd <jguitarok@gmail.com>
2024-07-30 17:00:00 +0300

    Solve sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    if (typeof(firstNumber) != "number" || typeof(secondNumber) != "number") {
+        return "ERROR";
+    }
+    if (!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber)) {
+        return "ERROR";
+    }
+    if (firstNumber<0 || secondNumber<0) {
+        return "ERROR";
+    }
+    let end = (firstNumber < secondNumber ? secondNumber: firstNumber);
+    let start = (firstNumber > secondNumber ? secondNumber: firstNumber);
+    let number = (start < end ? start : end);
+    let numberToAdd = number;
+    let i = 1;
+    while (i != end) {
+        numberToAdd += 1;
+        number += numberToAdd;
+        i += 1;
+    }
+    return number;
 };
 
 // Do not edit below this line
