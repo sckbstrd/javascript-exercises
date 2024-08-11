6937efe62af90239ee856ba030d5f0af19809723
sckbstrd <jguitarok@gmail.com>
2024-08-11 15:52:11 +0300

    Solve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    textArray = text.toLowerCase()
+                    .replace(/[^\w\s\']|_/g, "")
+                    .replace(/\s+/g, "")
+                    .split('');
+    if (textArray.join('') === textArray.reverse().join('')) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
