2534e486f72f4a4468296b5b69753f21302969c6
sckbstrd <jguitarok@gmail.com>
2024-08-11 16:57:52 +0300

    Solve fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(nth) { // nth number
+    
+    if (nth < 0) {
+        return "OOPS";
+    }
 
+    if (nth == 0) {
+        return 0;
+    } else if (nth == 1 || nth == 2) {
+        return 1;
+    } else {
+        result = 0;
+        for (let i=1; i<=2;) {
+            result += fibonacci(nth-i) 
+            i++;
+        }
+        
+        return result;
+    }
 };
 
 // Do not edit below this line
