4a86f5f5c5189bcd971976612624d81d650e4500
sckbstrd <jguitarok@gmail.com>
2024-08-11 15:33:06 +0300

    Solve calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, next) => acc + next, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((acc, next) => acc * next);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n == 0 || n == 1) {
+    return 1;
+  } else {
+    result = n * factorial(n-1);
+    return result;
+  }
 };
 
 // Do not edit below this line
@@ -31,3 +36,5 @@ module.exports = {
   power,
   factorial
 };
+
+
