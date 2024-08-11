e439c6c177d04b0565f5735191d39c53fff2cff0
sckbstrd <jguitarok@gmail.com>
2024-08-11 16:40:28 +0300

    Solve findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const currentYear = new Date().getFullYear();
+    const sortedByAge = arr.sort((a, b) => ((b.yearOfDeath || currentYear)- b.yearOfBirth) - ((a.yearOfDeath || currentYear)- a.yearOfBirth));
+    const oldest = sortedByAge[0];
+    return oldest;
 };
 
 // Do not edit below this line
